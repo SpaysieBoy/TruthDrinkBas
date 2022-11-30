4af50817442be958d2da2d6022c6e25005325cfa
Bas Dobbelstein <2063305dobbelstein@zuyd.nl>
2022-11-30 12:00:30 +0100

    Add project files.

---
+++ TruthDrinkBas/TruthDrinkBas/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TruthDrinkBas.Views;
+using Xamarin.Forms;
+
+namespace TruthDrinkBas
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            bool isUsernameEmpty = String.IsNullOrEmpty(UsernameEntry.Text);
+            bool isPasswordEmpty = String.IsNullOrEmpty(PasswordEntry.Text);
+
+            if (isUsernameEmpty)
+            {
+                UsernameEntry.Placeholder = "This can't be empty!";
+            }
+            else if (isPasswordEmpty)
+            {
+                PasswordEntry.Placeholder = "This can't be empty!";
+            }
+            else if (UsernameEntry.Text == "a" && PasswordEntry.Text == "a")
+            {
+                await Navigation.PushAsync(new HomePage());
+            }
+            else
+            {
+                _ = DisplayAlert("Ai", "Wrong Username or Password", "Ok");
+            }
+        }
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegisterPage());
+        }
+
+        private void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page3());
+        }
+
+
+    }
+}
---
+++ TruthDrinkBas/TruthDrinkBas/Views/HomePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TruthDrinkBas.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : TabbedPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void HelpToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new HelpPage());
+        }
+
+        private void LogOutToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+    }
+}
